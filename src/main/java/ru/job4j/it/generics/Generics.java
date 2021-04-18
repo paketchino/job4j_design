efdf4ae2e0e6d863410b58bc742df94f3073fd2c
paketchino <chansgorn@gmail.com>
2021-04-18 23:26:06 +0300

    0. Что такое обобщенные типы (generics) [#505466]

---
+++ src/main/java/ru/job4j/it/generics/Generics.java
@@ -0,0 +1,51 @@
+package ru.job4j.it.generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics gen = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal("Cat", 2));
+        second.add(new Predator("Tiger", 2));
+        third.add(new Tiger("Elephent", 32));
+        gen.printObject(first);
+        gen.printObject(second);
+        gen.printObject(third);
+        System.out.println();
+
+        gen.printBoundedWildCard(first);
+        gen.printBoundedWildCard(second);
+        gen.printBoundedWildCard(third);
+        System.out.println();
+
+        gen.printLowerBoundedCard(first);
+        gen.printLowerBoundedCard(second);
+        gen.printLowerBoundedCard(third);
+    }
+
+    public void printObject(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext(); ) {
+            Object next = it.next();
+            System.out.println("Текущий элемент " + next);
+        }
+    }
+
+    public void printBoundedWildCard(List<? extends Animal> list) {
+        for (Iterator<? extends Animal> it = list.iterator(); it.hasNext();) {
+            Animal next = it.next();
+            System.out.println("Текущий элемент " + next);
+        }
+    }
+
+    public void printLowerBoundedCard(List<? super Tiger> list) {
+        for (Iterator<? super Tiger> it = list.iterator(); it.hasNext(); ) {
+            Tiger next = (Tiger) it.next();
+            System.out.println("Текущий элемент " + next);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/it/generics/Tiger.java
@@ -0,0 +1,7 @@
+package ru.job4j.it.generics;
+
+public class Tiger extends Predator {
+    public Tiger(String name, int age) {
+        super(name, age);
+    }
+}
