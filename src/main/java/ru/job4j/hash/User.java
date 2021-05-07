6e95889be478ac23e0d07cf2a4bb0bfde5bb65bc
paketchino <chansgorn@gmail.com>
2021-05-07 11:09:27 +0300

    1. Создать модель User [#505471]

---
+++ src/main/java/ru/job4j/hash/User.java
@@ -0,0 +1,21 @@
+package ru.job4j.hash;
+
+import java.time.Month;
+import java.time.Year;
+import java.util.Calendar;
+
+import static java.util.Calendar.*;
+
+public class User {
+
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+}
