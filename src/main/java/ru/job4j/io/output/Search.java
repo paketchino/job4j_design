4df245da82bfc85c745a635f2d354404e0a7d7fc
paketchino <chansgorn@gmail.com>
2021-06-13 15:02:34 +0300

    5. Валидация параметров запуска. [#246865]

---
+++ src/main/java/ru/job4j/io/output/Search.java
@@ -0,0 +1,29 @@
+package ru.job4j.io.output;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        if (args.length == 1 || args[1].isEmpty()) {
+            throw new IllegalArgumentException("This expansion not found");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
