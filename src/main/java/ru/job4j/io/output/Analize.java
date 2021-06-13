ce46eabf8f9e02a592a4d54eba86f0520cd70ccb
paketchino <chansgorn@gmail.com>
2021-06-13 23:27:57 +0300

    2. Анализ доступности сервера. [#859 #164978]

--- src/main/java/ru/job4j/io/output/Analize.java
+++ src/main/java/ru/job4j/io/output/Analize.java
@@ -3,13 +3,13 @@ package ru.job4j.io.output;
 import java.io.*;
 
 public class Analize {
-    String add = null;
     public void unavailable(String source, String target) {
+        String add = null;
+        boolean isActive = true;
         try (BufferedReader br = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(
                      new BufferedOutputStream(
-                             new FileOutputStream(target)
-                     ))) {
+                             new FileOutputStream(target)   ))) {
             String read = br.readLine();
             while (read != null) {
                 if (!read.isEmpty()) {
@@ -18,8 +18,16 @@ public class Analize {
                         throw new IllegalArgumentException();
                     }
                     if (val[0].equals("400") || val[0].equals("500")) {
-                        add = val[1];
-                        out.printf(add + ";");
+                        if (isActive) {
+                            add = val[1];
+                            isActive = false;
+                        }
+                    }
+                    if (val[0].equals("200") || val[0].equals("300")) {
+                        if (!isActive) {
+                            out.printf(add + ";" + val[1] + ";");
+                            isActive = true;
+                        }
                     }
                 }
                 read = br.readLine();
