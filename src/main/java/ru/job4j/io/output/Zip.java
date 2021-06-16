0d3f8ae3a74238c7ff774cb7dc05c0e9c9bba8b2
paketchino <chansgorn@gmail.com>
2021-06-17 00:37:57 +0300

    5.2. Архивировать проект [#861 #168168]

--- src/main/java/ru/job4j/io/output/Zip.java
+++ src/main/java/ru/job4j/io/output/Zip.java
@@ -2,14 +2,16 @@ package ru.job4j.io.output;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<Path> source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+    public void packFiles(List<Path> source, Path target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toFile())))) {
             for (Path files : source) {
                 ZipEntry zFiles = new ZipEntry(files.toString());
                 zip.putNextEntry(zFiles);
@@ -33,10 +35,15 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
-        new Zip().packSingleFile(
-                new File("./chapter_005.xml"),
-                new File("./chapter_005.zip")
-        );
+    public static void main(String[] args) throws IOException {
+        ArgsName argsValues = ArgsName.of(args);
+        try {
+            List<Path> source = Search.search(
+                    Path.of(argsValues.getKey("d")),
+                    (file) -> !file.toFile().getName().endsWith("e"));
+            new Zip().packFiles(source, Path.of(argsValues.getKey("o")));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 }
