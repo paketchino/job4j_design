c4221069acb5832239c9730cb878c7912848987d
paketchino <chansgorn@gmail.com>
2021-06-22 20:30:08 +0300

    6. Кодировка. [#862 #170105]

--- src/main/java/ru/job4j/io/output/ConsoleChat.java
+++ src/main/java/ru/job4j/io/output/ConsoleChat.java
@@ -2,6 +2,7 @@ package ru.job4j.io.output;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,59 +13,56 @@ public class ConsoleChat {
     private static final String OUT = "out";
     private static final String STOP = "stop";
     private static final String CONTINUE = "continue";
-    private List<String> fileBot;
-    private List<String> log;
+    private final List<String> log = new ArrayList<>();
+    private final List<String> fileBot = new ArrayList<>();
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
         this.botAnswers = botAnswers;
     }
 
+    private List<String> readPhrases(String path) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path, Charset.forName("Windows-1251")))) {
+                reader.lines().forEach(sb::append);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileBot;
+    }
 
-    private void readFile() {
-        try (BufferedReader br = new BufferedReader(new FileReader(botAnswers,Charset.forName("Windows-1251")))) {
-            String read;
-            while ((read = br.readLine()) != null) {
-                fileBot.add(read);
+    private void writeDateInFile(String path, List<String> log) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("Windows-1251")))) {
+            for (String datum : log) {
+                pw.println(datum);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void writeDateInFile() {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("Windows-1251")))) {
-            log.forEach(pw::append);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String randomIndex() {
+    private String randomWords() {
         int randomIndex = (int) (Math.random() * fileBot.size() - 1);
         return fileBot.get(randomIndex);
     }
 
+
     public void run() {
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a phrases");
         boolean botCanAnswer = true;
-        readFile();
-        while (!sc.nextLine().equals(OUT)) {
-            String question = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String question = sc.nextLine();
+        readPhrases(fileBot.toString());
+        while (!question.equals(OUT)) {
+            randomWords();
             if (question.equals(STOP)) {
                 botCanAnswer = false;
+            }
+            if (!botCanAnswer) {
                 log.add(question);
             }
-            if (question.equals(CONTINUE)) {
-                botCanAnswer = true;
-                log.add(question);
-            }
-            if (botCanAnswer) {
-                randomIndex();
-                System.out.println(botAnswers);
-                log.add(botAnswers);
-            }
-            writeDateInFile();
+            writeDateInFile(question, log);
+            question = sc.nextLine();
         }
     }
 
