473c97d919e45194e1764d6cd8ef08eae8a814e4
paketchino <chansgorn@gmail.com>
2021-06-26 22:02:26 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/socket/EchoServer.java
+++ src/main/java/ru/job4j/socket/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.socket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +11,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -33,7 +39,7 @@ public class EchoServer {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Messages not found", e);
         }
     }
 }
