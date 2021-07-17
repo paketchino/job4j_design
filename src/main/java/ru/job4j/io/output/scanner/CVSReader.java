d0478f23f916d153993e11b1726105e9db49c06b
paketchino <chansgorn@gmail.com>
2021-07-17 22:20:32 +0300

    7. Scanner [#504791 #178140]

--- src/main/java/ru/job4j/io/output/scanner/CVSReader.java
+++ src/main/java/ru/job4j/io/output/scanner/CVSReader.java
@@ -4,9 +4,24 @@ import ru.job4j.io.output.ArgsName;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class CVSReader {
+
+    private Set<Integer> readHeader(String header, String filter) {
+        Set<Integer> tempStorage = new HashSet<>();
+        try (var scanner = new Scanner(new ByteArrayInputStream(header.getBytes()))) {
+            while (scanner.hasNext()) {
+                if (scanner.next().equals(filter)) {
+                    tempStorage.add(Integer.parseInt(scanner.next()));
+                }
+                scanner.close();
+            }
+        }
+        return tempStorage;
+    }
     public static void main(String[] args) throws IOException {
         ArgsName argsValues = ArgsName.of(args);
         var path = argsValues.getKey("path");
