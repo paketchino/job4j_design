025d553ef727ce1de85c67c69271fae0b0c55e71
paketchino <chansgorn@gmail.com>
2021-07-18 23:41:16 +0300

    7. Scanner [#504791 #178140]

--- src/main/java/ru/job4j/io/output/scanner/CVSReader.java
+++ src/main/java/ru/job4j/io/output/scanner/CVSReader.java
@@ -10,31 +10,39 @@ import java.util.Set;
 
 public class CVSReader {
 
-    private Set<Integer> readHeader(String header, String filter) {
-        Set<Integer> tempStorage = new HashSet<>();
-        try (var scanner = new Scanner(new ByteArrayInputStream(header.getBytes()))) {
+    private Set<Integer> readHeader(String header, String filter, String delimiter) {
+        Set<Integer> storageData = new HashSet<>();
+        try (var scanner = new Scanner(new ByteArrayInputStream(header.getBytes()))
+                .useDelimiter(delimiter)) {
             while (scanner.hasNext()) {
-                if (scanner.next().equals(filter)) {
-                    tempStorage.add(Integer.parseInt(scanner.next()));
+                String column = scanner.next();
+                String[] tempStorage = column.split(",");
+                for (int i = 0; i < tempStorage.length; i++) {
+                    if (tempStorage[i].equals(filter)) {
+                        int index = Integer.parseInt(tempStorage[i]);
+                        storageData.add(index);
+                    }
                 }
-                scanner.close();
             }
         }
-        return tempStorage;
+        return storageData;
     }
+
+    public void writeInFile(String out, Set<Integer> file) {
+        try(PrintWriter pw = new PrintWriter(new FileWriter(out))) {
+            file.forEach(pw::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        ArgsName argsValues = ArgsName.of(args);
-        var path = argsValues.getKey("path");
-        var delimiter = argsValues.getKey("delimiter");
-        try (Scanner scanner = new Scanner(new ByteArrayInputStream(path.getBytes())).useDelimiter(delimiter)) {
-            while (scanner.hasNext()) {
-                var result = scanner.findInLine(argsValues.getKey("filter"));
-                if (scanner.next().contains(result)) {
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(result);
-                    System.out.println(sb);
-                }
-            }
-       }
+        ArgsName argsValue = ArgsName.of(args);
+        CVSReader reader = new CVSReader();
+        reader.writeInFile(argsValue.getKey("out"),
+                reader.readHeader(argsValue.getKey("path"),
+                        argsValue.getKey("filter"),
+                        argsValue.getKey("delimiter")));
+
     }
 }
