d7521d6ad092204e873532c81a615142cf0dd9a8
paketchino <chansgorn@gmail.com>
2021-12-15 00:12:05 +0300

    3. Шаблонизатор. [#855]

--- src/main/java/ru/job4j/template/PhaseGeneration.java
+++ src/main/java/ru/job4j/template/PhaseGeneration.java
@@ -3,6 +3,7 @@ package ru.job4j.template;
 import java.util.Map;
 
 public class PhaseGeneration implements Generator {
+
     @Override
     public String produce(String template, Map<String, String> args) {
         return null;
---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenNeedToCheckPhase() {
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> storageMap = new HashMap<>();
+        storageMap.put("name", "Evgen");
+        storageMap.put("subject", "you");
+        String excepted = "I am a Evgen, Who are you?";
+        PhaseGeneration pg = new PhaseGeneration();
+        assertThat(pg.produce(template, storageMap), is(excepted));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNeedToCallNoSuchElementException() {
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> storageMap = new HashMap<>();
+        storageMap.put("project", "Amazon");
+        storageMap.put("input", "Computer");
+        PhaseGeneration pg = new PhaseGeneration();
+        pg.produce(template, storageMap);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNeedToCallIllegalArgumentException() {
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> storageMap = new HashMap<>();
+        storageMap.put("project", "Amazon");
+        storageMap.put("input", "Computer");
+        storageMap.put("name", "Evgen");
+        storageMap.put("subject", "you");
+    }
+}
