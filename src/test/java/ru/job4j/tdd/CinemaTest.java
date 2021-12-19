7e3158584bcc9a4514a43baba5302e28d2cf0e0d
paketchino <chansgorn@gmail.com>
2021-12-19 22:39:20 +0300

    2. Что такое TDD? [#4918 #240133]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -3,6 +3,7 @@ package ru.job4j.tdd;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
@@ -41,12 +42,46 @@ public class CinemaTest {
 
     @Ignore
     @Test
-    public void whenNeedToCheckDateAndPlaceBusyInSession() {
+    public void whenNeedToBuyTicketInvalidDate() {
         Account account = new AccountCinema();
         Cinema cinema = new ImplAccountCinema();
         Calendar date = Calendar.getInstance();
-        date.set(2020, 10, 10, 23, 00);
+        date.set(2020, -10, -10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-
+        assertNull(ticket);
     }
+
+    @Ignore
+    @Test
+    public void whenBuyTicketOnBusyPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new ImplAccountCinema();
+        Calendar dateOne = Calendar.getInstance();
+        Calendar dateSecond = Calendar.getInstance();
+        dateOne.set(2020, 10, 10, 23, 00);
+        dateSecond.set(2020, 10, 10, 23, 00);
+        Ticket ticketOne = cinema.buy(account, 1, 1, dateOne);
+        Ticket ticketSecond = cinema.buy(account, 1, 1, dateSecond);
+        assertNotNull(ticketOne);
+        assertNull(ticketSecond);
+    }
+
+    @Ignore
+    @Test
+    public void whenNeedToCreateSession() {
+        Cinema cinema = new ImplAccountCinema();
+        cinema.add(new Session3d());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3d())));
+    }
+
+    @Ignore
+    @Test
+    public void whenNeedToAddSessionToList() {
+        Session session = new Session3d();
+        List<Session> sessions = new ArrayList<>();
+        sessions.add(session);
+        assertThat(sessions, is(Arrays.asList(new Session3d())));
+    }
+
 }
