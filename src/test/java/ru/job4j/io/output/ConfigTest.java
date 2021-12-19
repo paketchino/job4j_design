3b686776a8a0ba00a320d31320126d05cf8b9ef7
paketchino <chansgorn@gmail.com>
2021-12-19 20:47:34 +0300

    Fix

--- src/test/java/ru/job4j/io/output/ConfigTest.java
+++ src/test/java/ru/job4j/io/output/ConfigTest.java
@@ -3,22 +3,15 @@ package ru.job4j.io.output;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
 public class ConfigTest {
-
-    @Test
-    public void whenPairWithoutComment() {
-        String path = "test.properties";
-        Config config = new Config(path);
-        config.load();
-        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
-        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
-        assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
-        assertThat(config.value("hibernate.connection.username"), is("postgres"));
-        assertThat(config.value("hibernate.connection.password"), is("password"));
-    }
 
     @Test
     public void whenFileWithComment() {
