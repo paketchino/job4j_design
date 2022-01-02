ac2901bbbae34b752c5536493f6262db5be3d40e
paketchino <chansgorn@gmail.com>
2022-01-02 23:24:08 +0300

    0. Принцип открытости закрытости [#4914 #246123]

--- src/main/java/ru/job4j/ocp/WrongOPC3.java
+++ src/main/java/ru/job4j/ocp/WrongOPC3.java
@@ -2,16 +2,17 @@ package ru.job4j.ocp;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.List;
 import java.util.Properties;
 
 public class WrongOPC3 {
 
     /*
-    Подобный подход мешает наследованию
-    хоть оно и не всегда применимо
+    Данный метод возвращаете null, что
+    является не правильным т.к метод ничего не возвращает
      */
 
-    public void load(Properties properties) {
+    public List load(Properties properties) {
         Connection cn;
         try (var loader = WrongOPC3.class.getClassLoader().getResourceAsStream("app.properties")) {
             Class.forName(properties.getProperty("jdbc.driver"));
@@ -22,5 +23,14 @@ public class WrongOPC3 {
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
+        return null;
+    }
+
+    /*
+    Метод принимает два параметра никак их не использует
+    и ничего не возвращет
+     */
+    public void getSum(int a, int b) {
+        System.out.println("Something doing");
     }
 }
