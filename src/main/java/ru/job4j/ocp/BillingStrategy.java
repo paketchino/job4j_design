d4d2b6f6d77af64c7f6d4d5435dadc73ee8cebc2
paketchino <chansgorn@gmail.com>
2022-01-02 16:17:19 +0300

    0. Принцип открытости закрытости [#4914] fix

--- src/main/java/ru/job4j/ocp/BillingStrategy.java
+++ src/main/java/ru/job4j/ocp/BillingStrategy.java
@@ -6,9 +6,9 @@ import java.util.function.IntUnaryOperator;
 
 enum BillingStrategy {
 
-        // Normal billing strategy (unchanged price)
+
         NORMAL (a -> a),
-        // Strategy for Happy hour (50% discount)
+
         HAPPY_HOUR (a -> a / 2);
 
         private final IntUnaryOperator strategy;
@@ -17,7 +17,6 @@ enum BillingStrategy {
             this.strategy = strategy;
         }
 
-        // Use a price in cents to avoid floating point round-off error
         int getActPrice(int rawPrice) {
             return this.strategy.applyAsInt(rawPrice);
         }
@@ -36,14 +35,12 @@ enum BillingStrategy {
             this.drinks.add(this.strategy.getActPrice(price * quantity));
         }
 
-        // Payment of bill
         public void print() {
             int sum = this.drinks.stream().mapToInt(v -> v).sum();
             System.out.println("Total due: " + sum);
             this.drinks.clear();
         }
 
-        // Set Strategy
         public void setStrategy(BillingStrategy strategy) {
             this.strategy = strategy;
         }
@@ -51,26 +48,22 @@ enum BillingStrategy {
 
      class StrategyPattern {
         public static void main(String[] arguments) {
-            // Prepare strategies
             BillingStrategy normalStrategy    = BillingStrategy.NORMAL;
             BillingStrategy happyHourStrategy = BillingStrategy.HAPPY_HOUR;
 
             CustomerBill firstCustomer = new CustomerBill(normalStrategy);
 
-            // Normal billing
             firstCustomer.add(100, 1);
 
-            // Start Happy Hour
             firstCustomer.setStrategy(happyHourStrategy);
             firstCustomer.add(100, 2);
 
-            // New Customer
             CustomerBill secondCustomer = new CustomerBill(happyHourStrategy);
             secondCustomer.add(80, 1);
-            // The Customer pays
+
             firstCustomer.print();
 
-            // End Happy Hour
+
             secondCustomer.setStrategy(normalStrategy);
             secondCustomer.add(130, 2);
             secondCustomer.add(250, 1);
